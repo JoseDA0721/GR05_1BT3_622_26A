0e57d36ac5566da10f1f7045ea1d5bd6c3dadfed
JoseDA0721 <133171655+JoseDA0721@users.noreply.github.com>
2026-04-19 19:23:19 -0500

    Test Unitario con Mocks: Persistencia del Servicio

--- src/main/java/org/redsaberes/service/ReviewService.java
+++ src/main/java/org/redsaberes/service/ReviewService.java
@@ -1,4 +1,8 @@
 package org.redsaberes.service;
 
+import org.redsaberes.model.Curso;
+import org.redsaberes.model.Usuario;
+
 public interface ReviewService {
+    void crearResena(Integer estrellas, String comentario, Usuario usuario, Curso curso);
 }
--- src/main/java/org/redsaberes/service/impl/ReviewServiceImpl.java
+++ src/main/java/org/redsaberes/service/impl/ReviewServiceImpl.java
@@ -1,17 +1,40 @@
 package org.redsaberes.service.impl;
 
+import org.redsaberes.model.Curso;
+import org.redsaberes.model.Resena;
+import org.redsaberes.model.Usuario;
 import org.redsaberes.repository.ReviewRepository;
 import org.redsaberes.repository.impl.ReviewRepositoryImpl;
 import org.redsaberes.service.ReviewService;
+import org.redsaberes.service.validator.ReviewValidator;
 
 public class ReviewServiceImpl implements ReviewService {
     private final ReviewRepository reviewRepository;
+    private final ReviewValidator reviewValidator;
 
     public ReviewServiceImpl() {
-        this(new ReviewRepositoryImpl());
+        this(new ReviewRepositoryImpl(), new ReviewValidator());
     }
 
-    ReviewServiceImpl(ReviewRepository reviewRepository) {
+    ReviewServiceImpl(ReviewRepository reviewRepository, ReviewValidator reviewValidator) {
         this.reviewRepository = reviewRepository;
+        this.reviewValidator = reviewValidator;
+    }
+
+    @Override
+    public void crearResena(Integer estrellas, String comentario, Usuario usuario, Curso curso) {
+        Resena resena = new Resena();
+
+        if (!reviewValidator.isValid(estrellas, comentario, usuario, curso)) {
+            return;
+        }
+
+        resena.setEstrellas(estrellas);
+        resena.setComentario(comentario);
+        resena.setUsuario(usuario);
+        resena.setCurso(curso);
+
+        reviewRepository.save(resena);
+
     }
 }
